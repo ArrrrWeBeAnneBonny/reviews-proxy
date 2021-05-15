c141d48ddad9e62f2784f0e90d044da454ad4435
Denise Jiang <denisejiang91@gmail.com>
2021-05-15 13:24:43 -0700

    proxy server up

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+app.use(morgan('dev'));
+
+const port = 3000;
+const host = 'localhost';
+let reviewsServer = 'http://localhost:3001';
+
+app.use('/reviews', createProxyMiddleware({
+  target: reviewsServer,
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/localhost:3001`]: '',
+  }
+}))
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log('error connecting to reviews proxy', err);
+  } else {
+    console.log(`connected to reviews proxy on ${port}`);
+  }
+});
+
+//curl http://localhost:3001/reviews?campId=0
